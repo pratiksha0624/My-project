f51f6d5af7602f2c4e6fbe3678a29fb46427aed8
pratiksha0624 <pratikshanikam006@gmail.com>
2025-05-31 09:16:27 +0530

    Add files via upload

---
+++ insertion of an array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int arr[50]={10,20,30,40,50};
+ 	int i=5;
+ 	int pos=2;
+ 	int item=25;
+ 	printf("element of an array is:\n");
+ 	for(i=0;i<=n;i++)
+ 	{
+ 		for(j=0;j<=n;j++)
+ 	}
+ 	{
+ 		printf("%d\n",arr[i]);
+ 	
+     }
+     int pos=item;
+     n++;//size increase 
+     {
+     	arr[i+1],arr[i];
+     }
+     {
+     	printf("element of an index:\n");
+     }
+     {
+     	printf("insertion of an array is %d%d,i,i++");
+     }
+     return 0;
+     
+ 	}
+ 	
+ 	
+ }
